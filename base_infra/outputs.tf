87beed755b6f648d1120ffad1a5106df5618f19f
rr6218a <34321627+rr6218a@users.noreply.github.com>
2020-09-04 23:30:59 +0530

    first base_infra commit

---
+++ base_infra/outputs.tf
@@ -0,0 +1,10 @@
+output "vpcid" {
+  description = "List of IDs of instances"
+  value       = ["${aws_vpc.default.id}"]
+}
+
+output "vpcname" {
+  description = "List of IDs of instances"
+  value       = ["${aws_vpc.default.tags.Name}"]
+}
+
